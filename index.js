9f85021a1a95cf46f71cec0a2701b3749781096b
mostafaeissa66t-eng <mostafaeissa66t@gmail.com>
2025-11-02 18:20:46 +0200

    index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get("/", (req, res) => {
 // !!! --- === === === === === === === === === === === === --- !!!
 // !!! --- الصق رابط جوجل آب سكريبت هنا بمنتهى الدقة --- !!!
 const GOOGLE_SCRIPT_URL =
-    "https://script.google.com/macros/s/AKfycbxBQ-wOAgNlB3C637_HcT_QhtPbnpF4EFJmFeTxT6NqUkDO13B5yrV5R2YrJ8oyvvpudg/exec";
+    "https://script.google.com/macros/s/AKfycbw0Qr4wRhrndOvPdKjS398OBSZkxXOSMe0zoMSBT-Ei4CAom-ZADg2xdS5ltT5JavD1XQ/exec";
 // !!! --- === === === === === === === === === === === === --- !!!
 
 // Log initial value
